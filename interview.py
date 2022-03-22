5c02487dd0f3229be314b02169a641942b1298f0
Cole Haraga <90408054+haragaCole@users.noreply.github.com>
2022-03-21 19:13:22 -0600

    Noarmalize experimental and chart box plots

--- interview.py
+++ interview.py
@@ -7,6 +7,12 @@
 import pandas as pd
 #numpy for numpy array types for the excel sheet to array
 import numpy as np
+# openpyxl for coordinates of excel sheets
+from openpyxl import Workbook
+
+# global constants
+rows = 12
+columns = 8
 
 # read excel files into a variable
 # excel and .py files must be in the same directory(folder) for this method
@@ -16,9 +22,30 @@ experimental1 = pd.read_excel(r'./../AAFC-interview/Exam_experimental_measuremen
 experimental2 = pd.read_excel(r'./../AAFC-interview/Exam_experimental_measurements2.xlsx', header=None)
 
 # put values into list
-# values are double array for each row into array
+# values are 2d list for each row into inner list i.e. [row][column]
 control1_list = control1.values.tolist()
 control2_list = control2.values.tolist()
 experimental1_list = experimental1.values.tolist()
 experimental2_list = experimental2.values.tolist()
+
+# need mean values of each control group
+mean_control1 = np.mean(control1_list)
+mean_control2 = np.mean(control2_list)
+
+# iterate through 2d experimental data set 1
+for i in range(rows):
+    for j in range(columns):
+        normalize1 = experimental1_list / mean_control1
+
+# iterate through 2d experimental data set 2
+for i in range(rows):
+    for j in range(columns):
+        normalize2 = experimental2_list /  mean_control2
+
+
+result = pd.DataFrame(normalize1) 
+result.to_excel('find_thresh_data1.xlsx')
+
+result = pd.DataFrame(normalize2) 
+result.to_excel('find_thresh_data2.xlsx')
 
